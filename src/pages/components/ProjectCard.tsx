196a7e16853248774ed9eb4ecce13e50923bcade
Brandon-Schefstad <bschefstad@gmail.com>
2023-05-23 20:10:00 -0400

    bug fixing mobile styles

--- src/pages/components/ProjectCard.tsx
+++ src/pages/components/ProjectCard.tsx
@@ -26,10 +26,10 @@ const ProjectCard = ({
   return (
     <section
       key={id}
-      className="projects relative mx-4 grid-cols-7 flex-col rounded-xl border-secondary bg-primaryLight  p-8 py-16 shadow-inner  shadow-secondary  dark:bg-accent-3 dark:shadow-accent-2 lg:px-12   "
+      className="projects relative  z-20 flex flex-col gap-8  border-secondary bg-accent-1 p-4 py-8 shadow-secondary  dark:bg-accent-3 dark:shadow-accent-2 lg:px-12   "
     >
       {" "}
-      <h2 className="rounded-xl bg-accent-3 px-14 py-4 text-2xl font-semibold text-primaryLight lg:w-3/4 lg:text-4xl">
+      <h2 className="rounded-xl   text-3xl font-semibold text-accent-3 lg:px-14  lg:text-4xl">
         {title}
       </h2>
       <section
@@ -39,42 +39,69 @@ const ProjectCard = ({
         <section className="flex flex-col gap-16 tracking-tight  text-secondary dark:text-primaryLight   lg:col-span-7">
           <section className="mx-2 flex flex-col gap-12 tracking-tighter lg:text-xl lg:tracking-normal">
             <section className="problem flex flex-col gap-4">
-              <h3 className=" text-xl lg:text-3xl">Problem</h3>
-              <span>{problem}</span>
+              <h3 className=" text-2xl font-bold tracking-wide lg:text-3xl">
+                Problem
+              </h3>
+              <span className={"block bg-primaryLight p-3 leading-5"}>
+                {problem}
+              </span>
             </section>
             <section className="solution flex flex-col gap-4">
-              <h3 className=" text-xl lg:text-3xl">Solution</h3>
-              <span>{solution}</span>
+              <h3 className=" text-2xl  font-bold tracking-wide lg:text-3xl">
+                Solution
+              </h3>
+              <span className={" block  bg-primaryLight p-3 leading-5"}>
+                {" "}
+                {solution}
+              </span>
             </section>
-            <section className="links flex flex-col gap-4">
-              <h3 className="  text-xl lg:text-3xl">Links</h3>
-              <section className="grid grid-cols-2 justify-start gap-8 text-center text-base text-secondary md:w-1/2 ">
+            <section className="links flex flex-col gap-4  ">
+              <h3 className="  text-2xl  font-bold tracking-wide lg:text-3xl">
+                Links
+              </h3>
+              <section className="grid grid-cols-2 justify-start  gap-6 bg-primaryLight p-3 text-center text-base tracking-[0.005em] text-secondary md:w-1/2 ">
                 <a
                   href={liveLink}
-                  className="pxl-4 rounded-xl bg-highlight py-2 font-bold "
+                  className=" rounded-xl bg-highlight px-4 py-2 font-bold  "
                 >
                   Live Site
                 </a>
                 <a
                   href={repoLink}
-                  className="rounded-xl  bg-highlight px-4 py-2 font-bold "
+                  className="rounded-xl  bg-highlight px-4 py-2 font-bold  "
                 >
                   Repo
                 </a>
               </section>{" "}
             </section>
             <section className="techStack flex flex-col gap-4">
-              <h3 className="text-xl lg:text-3xl">Stack</h3>
-              <ul className="   col-span-3 grid w-full grid-cols-3  gap-2 p-2  text-center  text-xs  lg:grid lg:gap-6  lg:px-28  lg:text-center lg:text-xl">
+              <h3 className="text-2xl font-bold tracking-wide lg:text-3xl">
+                Stack
+              </h3>
+              <ul className=" col-span-3 grid w-full  gap-2 bg-primaryLight p-3  text-center text-xs  tracking-[0.015em]  md:grid-cols-3  lg:grid lg:gap-6  lg:px-28  lg:text-center lg:text-xl">
                 {tech ? (
                   tech.map((techTool, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className="rounded-md border-2  border-solid  border-secondary  bg-accent-1 py-2 font-bold text-secondary dark:bg-primaryLight dark:text-secondary"
-                      >
-                        {techTool}
-                      </li>
+                    return index % 2 === 0 ? (
+                      <div className="grid grid-cols-2  justify-between gap-6">
+                        <li
+                          key={index}
+                          className="rounded-md border-2  border-solid  border-secondary  bg-accent-1 py-2 font-bold text-secondary dark:bg-primaryLight dark:text-secondary"
+                        >
+                          {techTool}
+                        </li>
+                        {tech[index + 1] ? (
+                          <li
+                            key={index + 1}
+                            className="rounded-md border-2  border-solid  border-secondary  bg-accent-1 py-2 font-bold text-secondary dark:bg-primaryLight dark:text-secondary"
+                          >
+                            {tech[index + 1]}
+                          </li>
+                        ) : (
+                          <></>
+                        )}
+                      </div>
+                    ) : (
+                      <></>
                     );
                   })
                 ) : (
@@ -84,8 +111,8 @@ const ProjectCard = ({
             </section>
           </section>
 
-          <section className=" flex flex-col gap-4 rounded-xl bg-accent-2 p-4 dark:bg-accent-3 dark:text-accent-1  lg:gap-16 lg:p-16 xl:grid xl:grid-cols-2">
-            <h2 className=" col-span-2 font-heading  text-2xl font-semibold  lg:mt-[0rem]  lg:text-4xl ">
+          <section className=" mx-2 flex flex-col gap-4 rounded-xl dark:text-accent-1  lg:gap-16 lg:p-16 xl:grid xl:grid-cols-2">
+            <h2 className=" col-span-2 font-heading  text-2xl font-bold  lg:mt-[0rem]  lg:text-4xl ">
               Gallery
             </h2>
             {sources.map((src, i) => {
--- src/pages/components/Technologies.tsx
+++ src/pages/components/Technologies.tsx
@@ -4,8 +4,8 @@ const Technologies = () => {
       <h2 className=" ml-4 pb-12 font-heading  text-5xl font-bold  text-secondary dark:text-primaryLight lg:text-6xl">
         Technologies
       </h2>
-      <section className="intro mx-4  flex flex-col gap-2 rounded-xl bg-accent-2  p-4 text-lg font-medium text-secondary dark:bg-white   lg:px-16 lg:py-14  ">
-        <section className="technologies  ml-[0rem] flex  flex-col gap-6 rounded-lg border-2 border-zinc-200 bg-white  py-12 shadow-lg shadow-zinc-600 dark:border-accent-2  dark:bg-secondary dark:bg-opacity-10 dark:shadow-zinc-600 lg:flex-row lg:gap-0 lg:px-2 lg:py-8 ">
+      <section className="intro mx-4  flex flex-col gap-2 rounded-xl bg-accent-2  p-6 text-lg font-medium text-secondary dark:bg-white   lg:px-16 lg:py-14  ">
+        <section className="technologies  ml-[0rem] flex  flex-col gap-6 rounded-lg border-2 border-zinc-200 bg-primaryLight  py-12 shadow-lg shadow-zinc-600 dark:border-accent-2  dark:bg-secondary dark:bg-opacity-10 dark:shadow-zinc-600 lg:flex-row lg:gap-0 lg:px-2 lg:py-8 ">
           <section className="technology-row flex w-full justify-evenly">
             <svg
               className="w-[50px] fill-secondary dark:fill-secondary "
